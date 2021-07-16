5538a404262069e623b393bbfc85b0b264859e6e
Sean <scoh21@basecampcodingacademy.org>
2021-07-16 13:41:03 -0500

    user account profile setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.contrib.auth import get_user_model
 from .forms import UserCreationForm
 from django.urls import reverse_lazy
 
@@ -8,3 +10,8 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class UserAccountDetailView(DetailView):
+    model = get_user_model()
+    template_name = 'user_account.html'
--- post/urls.py
+++ post/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('pants/', PantsListView.as_view(), name='pants_list'),
     path('shoes/', ShoesListView.as_view(), name='shoes_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-   
 ]
