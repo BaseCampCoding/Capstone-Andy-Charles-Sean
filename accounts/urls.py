1baf93bac480114e7e3997b8db8f71fc9b50a285
Sean <scoh21@basecampcodingacademy.org>
2021-07-19 11:55:30 -0500

    add selling and edit account

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignUpView, UserAccountDetailView
+from .views import SignUpView, UserAccountDetailView, UserAccountEdit, UserAccountSellingView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('account/<int:pk>/', UserAccountDetailView.as_view(), name='user_account')
+    path('account/<int:pk>/', UserAccountDetailView.as_view(), name='user_account'),
+    path('user/<int:pk>/edit', UserAccountEdit.as_view(), name='user_account_edit'),
+    path('account/<int:pk>/selling/', UserAccountSellingView.as_view(), name='items_selling')
 ]
